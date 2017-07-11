ceef5659cc9460e1f9b1ea99ca418b91b04c510f
Raymond Yee <raymond.yee@gmail.com>
2017-07-10 18:48:20 -0700

    I think you need to wrap the json data with a quote

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uncurl',
-    version='0.0.8',
+    version='0.0.8.1',
     description='A library to convert curl requests to python-requests.',
     author='Steve Pulec',
     author_email='spulec@gmail',
