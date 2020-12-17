8953ffc1191eff0f67725e5b4b8c5b24da0fa11c
zrthstr <zrth1k@gmail.com>
2020-12-17 23:46:21 +0100

    add --data-raw option as chrome started using it.\n in accordance with curls man page an alias to data-binary is sufficient.

--- tests/test_api.py
+++ tests/test_api.py
@@ -133,6 +133,15 @@ def test_parse_curl_with_binary_data():
 )"""
     )
 
+def test_parse_curl_with_raw_data():
+    uncurl.parse("""curl 'https://pypi.python.org/pypi/uncurl' --data-raw 'this is just some data'""").should.equal(
+        """requests.post("https://pypi.python.org/pypi/uncurl",
+    data='this is just some data',
+    headers={},
+    cookies={},
+    auth=(),
+)"""
+    )
 
 def test_parse_curl_with_another_binary_data():
     uncurl.parse("""curl -H 'PID: 20000079' -H 'MT: 4' -H 'DivideVersion: 1.0' -H 'SupPhone: Redmi Note 3' -H 'SupFirm: 5.0.2' -H 'IMEI: wx_app' -H 'IMSI: wx_app' -H 'SessionId: ' -H 'CUID: wx_app' -H 'ProtocolVersion: 1.0' -H 'Sign: 7876480679c3cfe9ec0f82da290f0e0e' -H 'Accept: /' -H 'BodyEncryptType: 0' -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; OPPO R9s Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36 hap/1.0/oppo com.nearme.instant.platform/2.1.0beta1 com.felink.quickapp.reader/1.0.3 ({"packageName":"com.oppo.market","type":"other","extra":{}})' -H 'Content-Type: text/plain; charset=utf-8' -H 'Host: pandahomeios.ifjing.com' --data-binary '{"CateID":"508","PageIndex":1,"PageSize":30}' --compressed 'http://pandahomeios.ifjing.com/action.ashx/otheraction/9028'""").should.equals(
@@ -204,6 +213,7 @@ if __name__ == '__main__':
     test_post_with_dict_data()
     test_post_with_string_data()
     test_parse_curl_with_binary_data()
+    test_parse_curl_with_raw_data()
     test_parse_curl_with_another_binary_data()
     test_parse_curl_with_insecure_flag()
     test_parse_curl_with_request_kargs()
